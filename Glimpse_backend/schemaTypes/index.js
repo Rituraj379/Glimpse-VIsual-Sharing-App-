b4cec02fa65af03323e276d81fa46a8f86e5c524
Rituraj379 <riturajs379@gmail.com>
2026-03-23 19:20:25 +0530

    Update README and remove legacy schema files

--- Glimpse_backend/schemaTypes/index.js
+++
@@ -1,7 +0,0 @@
-import user from './User'
-import comment from './Comment'
-import pin from './Pin'
-import postedBy from './PostedBy'
-import save from './Save'
-
-export const schemaTypes = [user, comment, pin, postedBy, save]
